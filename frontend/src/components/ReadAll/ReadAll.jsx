62db87d3e1f720ec67e3d65b3818166eb53ae11d
Luciano Citroni Lopes <luciano.citroni227@gmail.com>
2023-02-17 09:09:46 -0300

    removendo comentarios do exercicios

--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -7,23 +7,3 @@ function ReadAll(){
 
 
 export default ReadAll;
-
-
-
-
-//exercicio
-/*
-dentro do readall, crie um componenste de card
-dentro do card, exiba uma imagem qualquer (preferencia personagem)
-além disso, coloque o nome do personagem ou um texto
-
-não precisa ser integrado com o back
-
-
-2 - 
-pegue o div com a classe 'card' e extraia em um componente JSX, seguindo os mesmos passos
-que fizemos para a criação do readall
-
-
-
-*/
