1dfcb6ea05db2ea16f43f4a202ba0f8ce372af74
Luciano Citroni Lopes <luciano.citroni227@gmail.com>
2023-02-23 09:37:17 -0300

    criação do componente header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import ReadAll from './components/ReadAll/ReadAll'
+import Header from './components/Header/Header'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <ReadAll  />
     </div>
   )
