3cff8f01eeafe33dd2cf5c2f75225f188099c5f1
Luciano Citroni Lopes <luciano.citroni227@gmail.com>
2023-02-24 12:11:39 -0300

    implementando o ReadById

--- frontend/src/components/ReadById/ReadById.jsx
+++ frontend/src/components/ReadById/ReadById.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import Card from "../Card/Card";
+
 function ReadById(){
-    return <div>Return By Id</div>
+    const id = '63f7721a2531bdea397ea780';
+    const [item, setItem] = useState();
+
+
+    async function consultar(){
+        //const url = 'http://localhost:3000/item/'+id;
+        const url = `http://localhost:3000/item/${id}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+
+        setItem(data);
+
+    }
+
+    useEffect( function(){
+        consultar();
+    },[])
+    
+    
+    if(!item){
+        return <div>Carregando...</div>
+    }
+
+    
+    return <div className="ReadById"><Card item={item} /></div>
 }
 
 export default ReadById;
